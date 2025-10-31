96e906b9795e6b98606efb39c2027bccc2dad0fa
Narmadha52 <gunasekarnarmadha@gmail.com>
2025-10-31 13:16:51 +0530

    Update EnrollmentController.java

--- backend/src/main/java/com/lms/controller/EnrollmentController.java
+++ backend/src/main/java/com/lms/controller/EnrollmentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/enrollments")
+@RequestMapping("/enrollments")
 public class EnrollmentController {
 
     @Autowired
